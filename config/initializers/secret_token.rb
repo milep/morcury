5ee9745bd9b1107b0abfb814cfeaf4230cce402d
Miika Leppänen <maleppanen@gmail.com>
2012-11-27 17:17:25 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Morcury::Application.config.secret_token = '43608bb2623284660a66fee8b646e820c6bf0c0c45eefb84db676c27960ce3dd9bdde013ed9dc41e0d6cf3aacecd940b04a8b8badce9d2d1c3d514675a46cde8'

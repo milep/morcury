7041416b1ca0c044df6f9f8b51cd7e51a8aabb76
Miika Leppänen <maleppanen@gmail.com>
2013-02-14 17:06:32 +0200

    cleanup

--- app/assets/javascripts/page-properties.js
+++ app/assets/javascripts/page-properties.js
@@ -5,11 +5,3 @@ window.Mercury.Page.change_jumbotron_background = function(obj) {
   style += ') no-repeat bottom left;';
   $('#page_jumbotron_style').val(style);
 };
-
-window.Mercury.Page.moveUp = function(id) {
-  console.debug('id: %o', id);
-};
-
-window.Mercury.Page.moveDown = function(id) {
-  console.debug('id: %o', id);
-};
